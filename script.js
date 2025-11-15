1c2d3cf2d5628b1ccc04f641766b414a954ae042
Lambdadelta <176888319+Lambdadeltaaa@users.noreply.github.com>
2025-11-15 20:03:37 +0800

    code overhauled

--- script.js
+++ script.js
@@ -18,7 +18,14 @@ function extractTimetableData(timetableHTML) {
     tabularTimetable = timetables[1];
 
     classLocations = extractRoomLocation(graphicalTimetable);
-    console.log(classLocations);
+    classInfos = extractRemainingInformation(tabularTimetable);
+    
+    // enrich classInfos with their respective class locations
+    for (let i = 0; i < classInfos.length; i++) {
+        classInfos[i]["roomLocation"] = classLocations[classInfos[i]["courseCode"]][classInfos[i]["classType"]];
+    }
+
+    console.log(classInfos);
 }
 
 // Helper function for extractTimetableData.
@@ -28,12 +35,11 @@ function extractRoomLocation(graphicalTimetable) {
     let classLocations = {};
 
     for (let classUnit of classUnits) {
-        let textInfo = classUnit.querySelector("span").innerHTML;
+        let textInfo = classUnit.querySelector("span").innerHTML; // contains course code and class type
         let separator = textInfo.indexOf("(");
 
         let courseCode = textInfo.slice(0, separator);
         let classType = textInfo[separator + 1];
-
         let roomNumber = classUnit.firstChild.textContent;
 
         if (!classLocations[courseCode]) {
@@ -44,4 +50,44 @@ function extractRoomLocation(graphicalTimetable) {
     }
 
     return classLocations;
+}
+
+// Helper function for extractTimetableData.
+// Extract the remaining important infos for each of the class that the user have.
+function extractRemainingInformation(tabularTimetable) {
+    let tableRows = tabularTimetable.querySelector("tbody").rows;
+
+    let infoHeaders = ["number", "courseCode", "name", "classType", "classGroup", "attendance", "day", "time", "duration"];
+    let classInfos = [];
+
+    // i = 1 to skip the header
+    for (let i = 1; i < tableRows.length; i++) {
+        let tableCells = tableRows[i].cells;
+        let rowspan = tableCells[0].rowSpan ? +tableCells[0].rowSpan : 1;
+
+        let classInfo = {};
+        Array.from(tableCells).forEach((value, index) => {
+            let data = value.querySelector("a")?.innerHTML || value.innerHTML;
+            classInfo[infoHeaders[index]] = data;
+        });
+        classInfos.push({...classInfo});
+
+        // multiple rows share same certain values case
+        // the remaining rows in the rowspan only have day/time/duration values
+        // thus classInfo will be overwritten with these values in a loop
+        if (rowspan > 1) {
+            for (let j = i + 1; j < i + rowspan; j++) {
+                tableCells = tableRows[j].cells;
+                classInfo["day"] = tableCells[0].innerHTML;
+                classInfo["time"] = tableCells[1].innerHTML;
+                classInfo["duration"] = tableCells[2].innerHTML;
+
+                classInfos.push({...classInfo});
+            }
+
+            i += (rowspan - 1);
+        }
+    }
+
+    return classInfos;
 }
