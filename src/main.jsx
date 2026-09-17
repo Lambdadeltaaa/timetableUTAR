9c6ff6ceb76a43eecc109fa1ae590e5671cf6e07
Lambdadeltaaa <176888319+Lambdadeltaaa@users.noreply.github.com>
2026-09-17 23:24:55 +0800

    chore: remove /public dir and rename global css file name

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './index.css'
+import './main.css';
 
 document.body.setAttribute('data-bs-theme', 'dark');
 document.body.classList.add("main-bg");
